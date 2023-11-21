6372778258ec8009fc600299178f5d25d5a42aab
Francisco André <franciscoandre.farp@gmail.com>
2023-11-21 23:32:25 +0000

    scrollProgress update

---
+++ scripts/calcScrolTopButton.js
@@ -0,0 +1,21 @@
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("topButton");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+      scrollProgress.style.display = "grid";
+    } else {
+      scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+      document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#459591 ${scrollValue}%, #45959188 ${scrollValue}%)`;
+  };
+  
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
