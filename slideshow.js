7a59cc4ba318b656a531f1899c54cc48abac0152
Francisco André <franciscoandre.farp@gmail.com>
2023-11-21 11:52:27 +0000

    slideshow with some issues

--- slideshow.js
+++ slideshow.js
@@ -1,28 +1,52 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+let slideTimeout;
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+// Function to start the slideshow
+function startSlides() {
+  showSlides();
 }
 
+// Function to display slides
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  slideTimeout = setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// Function to pause the slideshow
+function pauseSlides() {
+  clearTimeout(slideTimeout); // Clear the timeout to pause the slideshow
+}
+
+// Function to resume the slideshow after a click event
+function resumeSlides() {
+  slideTimeout = setTimeout(showSlides, 2000); // Resume slideshow after 2 seconds
+}
+
+// Attach click event listener to each image to pause the slideshow
+let images = document.getElementsByClassName("mySlides");
+for (let i = 0; i < images.length; i++) {
+  images[i].addEventListener("click", function() {
+    pauseSlides();
+    // Resume slideshow after a delay (adjust the delay time as needed)
+    setTimeout(resumeSlides, 5000); // Resume after 5 seconds
+  });
+}
 function currentSlide(n) {
     showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("demo");
-    let captionText = document.getElementById("caption");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
-}
+  }
+// Start the slideshow when the page loads
+window.onload = function() {
+  startSlides();
+};
